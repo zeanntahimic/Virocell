b35aecc680d5ff0f863ae59fdc6e7225c3072691
Zeann Tahimic <zeann.tahimic@gmail.com>
2026-09-22 23:00:15 +0800

    def

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+class Cell:
+    def __init__(self, position:tuple, state: str, timer: int = 0):
+        self.position = position
+        self.state = state
+        self.timer = timer
